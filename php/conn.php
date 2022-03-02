baede948dfd6613e12b9ae7261e405d241a63cc3
Misael Lopez Ortiz <mloexer@gmail.com>
2022-03-02 12:10:41 -0500

    Conexión DB - php

---
+++ php/conn.php
@@ -0,0 +1,14 @@
+<?php
+$db_usuario = "admin";
+$db_pswr = "ppyOthVr9n796ClyNZzm";
+$db_base_datos = "sys";
+$db_equipo = "pruebas-misael.cikb6np6vwrk.us-east-1.rds.amazonaws.com";
+
+$connection = new mysql($db_equipo, $db_usuario, $db_pswr, $db_base_datos);
+
+mysql_set_charset($connection, 'utf8');
+
+if($connection) {
+    $id_hilo = mysql_thread_id($connection);
+    printf("Se conecto la base con el ID: %d", $id_hilo);
+}
